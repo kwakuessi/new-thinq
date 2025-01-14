55d2cfc56f37de1f057a612191e82a6e4f42d95c
Amedzro Emmanuel <hansonnewton@gmail.com>
2025-01-14 16:36:20 +0000

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,6 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
+    // 'disk_name' => env('MEDIA_DISK', 'public'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -6,5 +6,7 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './vendor/ralphjsmit/laravel-filament-media-library/resources/**/*.blade.php'
+
     ],
 }
