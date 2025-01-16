bd059b5169829894f61f31b4a5c462d40cda5636
Emmanuel Esiiaw <kayessiaw@gmail.com>
2025-01-16 16:27:45 +0000

    16/01/25 clossing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,10 +7,12 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
 
         <!-- Styles / Scripts -->
+        <script src="https://unpkg.com/alpinejs" defer></script>
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
@@ -19,8 +21,14 @@
             </style>
         @endif
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body class="font-Lexend font-light text-md antialiased dark:text-white/50">
+    <x-nav.nav/>
+>
         {{ $slot }}
+
+
+
         @vite('resources/js/app.js')
+        <x-footer.footer/>
     </body>
 </html>
---
+++ resources/views/components/titles/icont-title.blade.php
@@ -0,0 +1,7 @@
+<div class="w-[236px] h-[87.18px] relative">
+  <div class="w-[103px] h-[87.18px] left-0 top-0 absolute">
+    <div class="w-[69.03px] h-[69.03px] left-0 top-0 absolute bg-gradient-to-tl from-[#fbba61] to-white rounded-full"></div>
+    <div class="w-[69.03px] h-[69.03px] left-[24.45px] top-[18.93px] absolute origin-top-left rotate-[-8.58deg] bg-gradient-to-tl from-white via-[#facdbf] to-[#f15928] rounded-full"></div>
+  </div>
+  <div class="left-[16px] top-[14px] absolute text-[#1d4289] text-4xl font-normal font-['Lexend']">[{{ $title }}]</div>
+</div>
